e991473f101505f7bcb1b4a64c4e123f0c6c0b7f
efg.river <efg.river@gmail.com>
2018-06-23 11:05:52 +0900

    POST /scores/createアクションとindex.html側のajax呼び出しコードを作成

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,4 +8,19 @@ class ScoresController < ApplicationController
         {:user_id => 3, :user_name => "甘栗太郎", :score => 40}
     ];
   end
+  def create
+
+    # リクエストパラメータを取得
+    score = params[:score]
+    user_id = params[:user_id]
+    
+    # Resultクラス(resultテーブル用のモデル)のcreateメソッドを実行・DB上のresultテーブルにレコードを新規登録
+    result = Result.create({:user_id => user_id, :score => score });
+
+    respond_to do |format|
+      format.json { 
+        render :json => { status: "ok", result: result.attributes }
+      } # ここを修正してます
+    end
+  end
 end
